486730e736879fb6883beda6e7225fdafde315b1
kimberly burton <kj.burton03@gmail.com>
2023-07-25 22:18:15 -0400

    fixed routes still need to fix box

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -25,11 +25,11 @@ export const ApplicationViews = () => {
 
         <Route path="/todos" element={<TodoList />} />
         <Route path="/todoform" element={<TodoForm />} />
-        <Route path="/todos/edit/:todoId" element={<TodoEdit />} />
+        <Route path="/todos/editShop/:todoId" element={<TodoEdit />} />
 
         <Route path="/shops" element={<ShopList />} />
         <Route path="/shopform" element={<ShopForm />} />
-        <Route path="/shops/edit/:shopId" element={<ShopEdit />} />
+        <Route path="/shops/editshop/:shopId" element={<ShopEdit />} />
 
         <Route path="/home" element={<Home />} />
       </Route>
